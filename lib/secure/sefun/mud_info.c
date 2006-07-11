a6a5491e31dcb77479dd8fa7a5d80a008031683d
Dread Quixadhal <quixadhal@gmail.com>
2006-07-11 16:24:14 -0400

    ds2.00r31

--- lib/secure/sefun/mud_info.c
+++ lib/secure/sefun/mud_info.c
@@ -27,7 +27,7 @@ string architecture() { return __ARCH__; }
 
 string mudlib() { return "Dead Souls"; }
 
-string mudlib_version() { return "2.1a2"; }
+string mudlib_version() { return "2.1a3"; }
 
 int query_host_port() { return __PORT__; }
 
