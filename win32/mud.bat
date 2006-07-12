d492d1269fc27a1fa70977ccf51c8efbd179caea
Dread Quixadhal <quixadhal@gmail.com>
2006-07-12 12:12:20 -0400

    ds2.01r07

---
+++ win32/mud.bat
@@ -0,0 +1,7 @@
+# This improved batch file courtesy of Brodbane @ Eve
+goto start
+:start
+c:\ds\win32\driver.exe c:\ds\lib\secure\cfg\mudos.cfg
+# Add a '#' to the following line to prevent auto-restarts
+goto start
+:end
