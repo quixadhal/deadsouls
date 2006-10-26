2577fe6315e44031269ae0ce46845f4686a2d05b
Dread Quixadhal <quixadhal@gmail.com>
2006-10-26 19:47:20 -0400

    ds2.01r53

--- lib/secure/daemon/i3router/funcs.h
+++ lib/secure/daemon/i3router/funcs.h
@@ -3,8 +3,8 @@
 static mapping muds_on_this_fd(int fd){
     return filter_mapping(connected_muds,"value_equals",this_object(),fd);
 }
-int value_equals(string a,int b, int c){ return b==c; }
+int value_equals(string a,int b, int c){ true(a); return b==c; }
 static mapping muds_not_on_this_fd(int fd){
     return filter_mapping(connected_muds,"value_equals",this_object(),fd);
 }
-int value_not_equals(string a,int b, int c){ return b!=c; }
+int value_not_equals(string a,int b, int c){ true(a); return b!=c; }
--- lib/secure/daemon/i3router/send_chanlist_reply.h
+++ lib/secure/daemon/i3router/send_chanlist_reply.h
@@ -4,7 +4,7 @@ static void send_chanlist_reply(string mudname, int old_chanid){
     string channame; // channel name
     //	string *output; // array of which channel names to tell them about
     mapping out=([ ]); // mapping to send
-    int i; // counter
+    //int i; // counter
     trr("send_chanlist_reply, mudname="+mudname+", old_chanid="+old_chanid);
     foreach(channame in keys(channel_updates)){
 	//#ifndef SEND_WHOLE_CHANLIST
