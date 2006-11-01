d003992833be117d9ba849b1ce4915810ced0b42
Dread Quixadhal <quixadhal@gmail.com>
2006-10-31 20:29:37 -0500

    ds2.01r59

--- lib/lib/events/look.c
+++ lib/lib/events/look.c
@@ -166,9 +166,9 @@ mapping RemoveItem(mixed item) {
 }
 
 mapping SetItems(mapping items) {
-foreach(mixed key, mixed val in items){
-AddItem(key, val);
-}
+    foreach(mixed key, mixed val in items){
+        AddItem(key, val);
+    }
     return copy(Items);
 }
 
--- lib/lib/std/room.c
+++ lib/lib/std/room.c
@@ -825,12 +825,14 @@ varargs mixed eventHearTalk(object who, object target, int cls, string verb,
 	return 1;
 
     case TALK_LOCAL:
-	obs = get_livings(this_object(),1) - ({ who });
-	if(sizeof(obs))
-	    obs->eventHearTalk(who, target, cls, verb, msg, lang);
-	obs = get_livings(this_object(),2) - ({who});
-	if(sizeof(obs))
-	    obs->eventHearTalk(who, target, cls, verb, msg, lang);
+	obs = get_livings(this_object(),1);
+        if(sizeof(obs)) obs -= ({ who });
+        if(sizeof(obs))
+	obs->eventHearTalk(who, target, cls, verb, msg, lang);
+        obs = get_livings(this_object(),2);
+        if(sizeof(obs)) obs -= ({ who });
+        if(sizeof(obs))
+	obs->eventHearTalk(who, target, cls, verb, msg, lang);
 	return 1;
 
     case TALK_AREA:
